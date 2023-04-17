e90d344f980191a0242fd190fede127745ac8f59
Muhammad Yar <ranamuhammadyaar9@gmail.com>
2023-04-17 20:23:33 +0500

    reactapp

--- src/components/News.js
+++ src/components/News.js
@@ -1,80 +1,101 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
-  articles =  [
+  articles = [
     {
-      "source": {
-        "id": null,
-        "name": "Lifehacker.com"
+      source: {
+        id: null,
+        name: "Gizmodo.com",
       },
-      "author": "Katharine Trendacosta on Two Cents, shared by Katharine Trendacosta to Lifehacker",
-      "title": "How to Get a Free Credit Report",
-      "description": "Your credit report is a snapshot of your financial life. It’s a record of your credit history, including your credit cards, loans, and other debts. It’s also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if you’re a…",
-      "url": "https://twocents.lifehacker.com/how-to-get-a-free-credit-report-1846640001",
-      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/s--_5Z7Z7Zg--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1b0b0f5d0f1b0f1f.jpg",
-      "publishedAt": "2021-01-04T19:00:00Z",
-      "content": "Your credit report is a snapshot of your financial life. Its a record of your credit history, including your credit cards, loans, and other debts. Its also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if youre a… [+2507 chars]"
+      author: "Kyle Barr",
+      title: "Here’s How to Find the Original Bitcoin Manifesto in Your Mac",
+      description:
+        "An Apple developer left one of the strangest Easter eggs hidden in the macOS, offering a surprising and baffling connection between the Cupertino tech company’s mainline operating system to everybody’s (least) favorite cryptographic digital currency. Read mor…",
+      url: "https://gizmodo.com/bitcoin-apple-mac-bitcoin-white-paper-1850307069",
+      urlToImage:
+        "https://i.insider.com/6417b59ffa3bcb001907f1a8?width=1200&format=jpeg",
+      publishedAt: "2023-04-06T15:30:00Z",
+      content:
+        "An Apple developer left one of the strangest Easter eggs hidden in the macOS, offering a surprising and baffling connection between the Cupertino tech companys mainline operating system to everybodys… [+3865 chars]",
     },
     {
-      "source": {
-        "id": null,
-        "name": "Lifehacker.com"
+      source: {
+        id: "google-news",
+        name: "Google News",
       },
-      "author": "Katharine Trendacosta on Two Cents, shared by Katharine Trendacosta to Lifehacker",
-      "title": "How to Get a Free Credit Report",
-      "description": "Your credit report is a snapshot of your financial life. It’s a record of your credit history, including your credit cards, loans, and other debts. It’s also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if you’re a…",
-      "url": "https://twocents.lifehacker.com/how-to-get-a-free-credit-report-1846640001",
-      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/s--_5Z7Z7Zg--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1b0b0f5d0f1b0f1f.jpg",
-      "publishedAt": "2021-01-04T19:00:00Z",
-      "content": "Your credit report is a snapshot of your financial life. Its a record of your credit history, including your credit cards, loans, and other debts. Its also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if youre a… [+2507 chars]"
+      author: null,
+      title: "Cryptoverse: Bitcoin traders like their options - Reuters",
+      description: "Cryptoverse: Bitcoin traders like their options  Reuters",
+      url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMiXWh0dHBzOi8vd3d3LnJldXRlcnMuY29tL3RlY2hub2xvZ3kvY3J5cHRvdmVyc2UtYml0Y29pbi10cmFkZXJzLWxpa2UtdGhlaXItb3B0aW9ucy0yMDIzLTA0LTA0L9IBAA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+      urlToImage:
+        "https://i.guim.co.uk/img/media/557a4b7ba8d4a7ac4ffdc68894f2b3caa276f344/0_235_3551_2132/master/3551.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=56808976844276c471575e281886d474",
+      publishedAt: "2023-04-04T05:18:00Z",
+      content:
+        "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
     },
     {
-      "source": {
-        "id": null,
-        "name": "Lifehacker.com"
+      source: {
+        id: "business-insider",
+        name: "Business Insider",
       },
-      "author": "Katharine Trendacosta on Two Cents, shared by Katharine Trendacosta to Lifehacker",
-      "title": "How to Get a Free Credit Report",
-      "description": "Your credit report is a snapshot of your financial life. It’s a record of your credit history, including your credit cards, loans, and other debts. It’s also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if you’re a…",
-      "url": "https://twocents.lifehacker.com/how-to-get-a-free-credit-report-1846640001",
-      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/s--_5Z7Z7Zg--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1b0b0f5d0f1b0f1f.jpg",
-      "publishedAt": "2021-01-04T19:00:00Z",
-      "content": "Your credit report is a snapshot of your financial life. Its a record of your credit history, including your credit cards, loans, and other debts. Its also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if youre a… [+2507 chars]"
+      author: "Carla Mozée",
+      title:
+        "Michael Saylor's MicroStrategy adds to its bitcoin trove, purchasing 1,045 coins for $24 million",
+      description:
+        "The business software maker now holds 140,000 bitcoin and the value of that stash was nearly $4 billion with bitcoin trading above $28,000.",
+      url: "https://markets.businessinsider.com/news/currencies/bitcoin-microstrategy-cryptocurrency-michael-saylor-coins-buys-holding-value-mstr-2023-4",
+      urlToImage:
+        "https://i.insider.com/631133643fe7c40019e4f2fd?width=1200&format=jpeg",
+      publishedAt: "2023-04-05T16:47:03Z",
+      content:
+        "Michael Saylor is facing a $100 million lawsuit for tax evasionMarco Bello/Getty Images\r\n<ul>\n<li>MicroStrategy continued to build its bitcoin stash in early April, buying 1,045 coins. </li>\n<li>It b… [+1587 chars]",
     },
     {
-      "source": {
-        "id": null,
-        "name": "Lifehacker.com"
+      source: {
+        id: "google-news",
+        name: "Google News",
       },
-
-      "author": "Katharine Trendacosta on Two Cents, shared by Katharine Trendacosta to Lifehacker",
-      "title": "How to Get a Free Credit Report",
-      "description": "Your credit report is a snapshot of your financial life. It’s a record of your credit history, including your credit cards, loans, and other debts. It’s also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if you’re a…",
-      "url": "https://twocents.lifehacker.com/how-to-get-a-free-credit-report-1846640001",
-      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/s--_5Z7Z7Zg--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1b0b0f5d0f1b0f1f.jpg",
-      "publishedAt": "2021-01-04T19:00:00Z",
-      "content": "Your credit report is a snapshot of your financial life. Its a record of your credit history, including your credit cards, loans, and other debts. Its also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if youre a… [+2507 chars]"
+      author: null,
+      title:
+        "Bitcoin near 9-month high as bank turmoil sparks rally - Reuters.com",
+      description:
+        "Bitcoin near 9-month high as bank turmoil sparks rally  Reuters.com",
+      url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMiamh0dHBzOi8vd3d3LnJldXRlcnMuY29tL21hcmtldHMvY3VycmVuY2llcy9iaXRjb2luLW5lYXItOS1tb250aC1oaWdoLWJhbmstdHVybW9pbC1zcGFya3MtcmFsbHktMjAyMy0wMy0yMC_SAQA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+      urlToImage:
+        "https://i.insider.com/6430035a3007470019fe1513?width=1200&format=jpeg",
+      publishedAt: "2023-03-20T06:52:00Z",
+      content:
+        "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
     },
-  ]
-  constructor (){
+  ];
+  constructor() {
     super();
-    console.log("Hello I am a constructor from News Component");
     this.state = {
       articles: this.articles,
-      loading: false
-      
+      loading: false,
+    };
   }
-}
   render() {
     return (
       <div>
-        This is a news component
-        <NewsItems title = "demo" description = "test" />
-        <NewsItems title = "XYZ" description = "ABC" />
+        <h2 className="text-center">Today News</h2>
+        <div className="container py-2 row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4 py-3" key ={element.url} >
+                <NewsItems
+                  title={element.title}
+                  description={element.description}
+                  urlToImage={element.urlToImage}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,20 +2,18 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    let { title,description,imageUrl,newsUrl,} = this.props;
+    let { title,description,urlToImage} = this.props;
     return (
       <div>
         <div className="container">
           <div className="row">
-            <h2>All News</h2>
             <div className="col-md-4">
-              <div className="card" style={{ width: "18rem;" }}>
-                <img src="..." className="card-img-top" alt="..." />
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={urlToImage} className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">{title}</h5>
                   <p className="card-text">{description}</p>
-                  
-                  <a href="#" className="btn btn-primary">
+                  <a href="/go" className="btn btn-primary">
                     Go somewhere
                   </a>
                 </div>
