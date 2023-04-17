c63fe67725859dad7bbaf45ff56fd725bf313dac
Muhammad Yar <ranamuhammadyaar9@gmail.com>
2023-04-17 17:08:50 +0500

    first

---
+++ src/components/NewsItems.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+  render() {
+    return (
+      <div>
+        NewsItem Component 1
+        
+      </div>
+    )
+  }
+}
+
+export default NewsItems
