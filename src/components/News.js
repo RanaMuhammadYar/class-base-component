fb362feeed3df8bbd45797355c1cb78807551eda
Muhammad Yar <ranamuhammadyaar9@gmail.com>
2023-04-17 19:35:05 +0500

    api call

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,76 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 
 export class News extends Component {
+  articles =  [
+    {
+      "source": {
+        "id": null,
+        "name": "Lifehacker.com"
+      },
+      "author": "Katharine Trendacosta on Two Cents, shared by Katharine Trendacosta to Lifehacker",
+      "title": "How to Get a Free Credit Report",
+      "description": "Your credit report is a snapshot of your financial life. It’s a record of your credit history, including your credit cards, loans, and other debts. It’s also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if you’re a…",
+      "url": "https://twocents.lifehacker.com/how-to-get-a-free-credit-report-1846640001",
+      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/s--_5Z7Z7Zg--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1b0b0f5d0f1b0f1f.jpg",
+      "publishedAt": "2021-01-04T19:00:00Z",
+      "content": "Your credit report is a snapshot of your financial life. Its a record of your credit history, including your credit cards, loans, and other debts. Its also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if youre a… [+2507 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Lifehacker.com"
+      },
+      "author": "Katharine Trendacosta on Two Cents, shared by Katharine Trendacosta to Lifehacker",
+      "title": "How to Get a Free Credit Report",
+      "description": "Your credit report is a snapshot of your financial life. It’s a record of your credit history, including your credit cards, loans, and other debts. It’s also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if you’re a…",
+      "url": "https://twocents.lifehacker.com/how-to-get-a-free-credit-report-1846640001",
+      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/s--_5Z7Z7Zg--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1b0b0f5d0f1b0f1f.jpg",
+      "publishedAt": "2021-01-04T19:00:00Z",
+      "content": "Your credit report is a snapshot of your financial life. Its a record of your credit history, including your credit cards, loans, and other debts. Its also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if youre a… [+2507 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Lifehacker.com"
+      },
+      "author": "Katharine Trendacosta on Two Cents, shared by Katharine Trendacosta to Lifehacker",
+      "title": "How to Get a Free Credit Report",
+      "description": "Your credit report is a snapshot of your financial life. It’s a record of your credit history, including your credit cards, loans, and other debts. It’s also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if you’re a…",
+      "url": "https://twocents.lifehacker.com/how-to-get-a-free-credit-report-1846640001",
+      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/s--_5Z7Z7Zg--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1b0b0f5d0f1b0f1f.jpg",
+      "publishedAt": "2021-01-04T19:00:00Z",
+      "content": "Your credit report is a snapshot of your financial life. Its a record of your credit history, including your credit cards, loans, and other debts. Its also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if youre a… [+2507 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Lifehacker.com"
+      },
+
+      "author": "Katharine Trendacosta on Two Cents, shared by Katharine Trendacosta to Lifehacker",
+      "title": "How to Get a Free Credit Report",
+      "description": "Your credit report is a snapshot of your financial life. It’s a record of your credit history, including your credit cards, loans, and other debts. It’s also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if you’re a…",
+      "url": "https://twocents.lifehacker.com/how-to-get-a-free-credit-report-1846640001",
+      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/s--_5Z7Z7Zg--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1b0b0f5d0f1b0f1f.jpg",
+      "publishedAt": "2021-01-04T19:00:00Z",
+      "content": "Your credit report is a snapshot of your financial life. Its a record of your credit history, including your credit cards, loans, and other debts. Its also a record of your payment history, which is a big factor in your credit score. Your credit report is used by lenders to determine if youre a… [+2507 chars]"
+    },
+  ]
+  constructor (){
+    super();
+    console.log("Hello I am a constructor from News Component");
+    this.state = {
+      articles: this.articles,
+      loading: false
+      
+  }
+}
   render() {
     return (
       <div>
         This is a news component
-        <NewsItems/>
+        <NewsItems title = "demo" description = "test" />
+        <NewsItems title = "XYZ" description = "ABC" />
       </div>
     )
   }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
+    let { title,description,imageUrl,newsUrl,} = this.props;
     return (
       <div>
-        NewsItem Component 1
-        
+        <div className="container">
+          <div className="row">
+            <h2>All News</h2>
+            <div className="col-md-4">
+              <div className="card" style={{ width: "18rem;" }}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{title}</h5>
+                  <p className="card-text">{description}</p>
+                  
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItems
+export default NewsItems;
